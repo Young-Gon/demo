9d4f477c5022b8cbc83535aa17593aed738aea85
yg1027 <yg1028@seedit.co.kr>
2018-12-14 17:08:14 +0900

    디비를 시드 디비로 변경

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,12 +1,15 @@
 package com.example.demo
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude = [DataSourceAutoConfiguration::class])
 class DemoApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
 }
-
