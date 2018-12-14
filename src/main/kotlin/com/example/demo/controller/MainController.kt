d3b7940e381e637df312f823a96dcae782bdd8ab
yg1027 <yg1028@seedit.co.kr>
2018-12-14 16:12:54 +0900

    The server time zone value '´ëÇÑ¹Î±¹ Ç¥ÁØ½Ã' is unrecognized or represents more than one time zone.

--- src/main/kotlin/com/example/demo/controller/MainController.kt
+++ src/main/kotlin/com/example/demo/controller/MainController.kt
@@ -1,35 +1,16 @@
 package com.example.demo.controller
 
-import com.example.demo.service.MainService
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+
+@Controller
 class MainController {
 
-    @Autowired
-    lateinit var mainService: MainService
-
-    @GetMapping("personList")
-    fun personList() = mainService.getPersonList()
-
-    @PostMapping("insertPerson")
-    fun insertPerson(name:String, country:String)=with(mainService) {
-        insertPerson(name,country)
-    }
-
-    @GetMapping("orderList")
-    fun orderList(personId:Int) = mainService.getOrderList(personId)
-
-    @PostMapping("insertOrder")
-    fun insertOrder(orderName:String, person_id:Int): ResponseEntity<*> {
-        val orderEntity = mainService.insertOrder(orderName, person_id)
-        if (orderEntity == null)
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)
-        return ResponseEntity.ok().body(orderEntity)
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute("eventName", "FIFA 2018")
+        return "hello"
     }
 }
---
+++ src/main/kotlin/com/example/demo/controller/RestController.kt
@@ -0,0 +1,35 @@
+package com.example.demo.controller
+
+import com.example.demo.service.MainService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class RestController {
+
+    @Autowired
+    lateinit var mainService: MainService
+
+    @GetMapping("personList")
+    fun personList() = mainService.getPersonList()
+
+    @PostMapping("insertPerson")
+    fun insertPerson(name:String, country:String)=with(mainService) {
+        insertPerson(name,country)
+    }
+
+    @GetMapping("orderList")
+    fun orderList(personId:Int) = mainService.getOrderList(personId)
+
+    @PostMapping("insertOrder")
+    fun insertOrder(orderName:String, person_id:Int): ResponseEntity<*> {
+        val orderEntity = mainService.insertOrder(orderName, person_id)
+        if (orderEntity == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)
+        return ResponseEntity.ok().body(orderEntity)
+    }
+}
